a04b7bceaa4f8bc4adb0131e878b6c81cc2c2278
suracho <suracho@outlook.com>
2021-04-27 17:48:16 +0530

    tp

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.set('view engine','ejs')
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.set('view engine','ejs');
 
-app.listen(port)
+app.listen(port, ()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
 
-app.use(express.static('public'))
-app.use(morgan('dev'))
 
-app.get('/'),(req, res)=> {
-    
-}
+
+app.get('/',(req, res)=> {
+    res.render('index');   
+});
+app.get('/login',(req, res)=> {
+ res.render('login');   
+});
+app.get('/registration',(req, res)=> {
+    res.render('registration');   
+});
+
+app.use((req,res)=>{
+    res.status(404).render('404');
+});
