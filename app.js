7578d9b3486b47d95bd55a36e4ae21123f929509
suracho <suracho@outlook.com>
2021-04-27 22:58:15 +0530

    added registration function

--- app.js
+++ app.js
@@ -1,27 +1,70 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-app.use(morgan('dev'));
+
+const dbUri = 'mongodb+srv://butter:butter123@bloodbank.wits5.mongodb.net/bloodbank?retryWrites=true&w=majority';
+mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result)=> app.listen(port, ()=>{
+        console.log(`Example app listening at http://localhost:${port}`);
+    }))
+    .catch((err) => console.log(err));
+//register view engine
 app.set('view engine','ejs');
 
-app.listen(port, ()=>{
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+//middleware and static
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
-
+app.use(express.urlencoded({extended : true}));
 
 app.get('/',(req, res)=> {
-    res.render('index');   
+    res.redirect('/login');   
 });
+
 app.get('/login',(req, res)=> {
  res.render('login');   
 });
+
+app.post('/login',(req , res)=>{
+    console.log(req.body);
+});
+
 app.get('/registration',(req, res)=> {
+   
     res.render('registration');   
+});
+
+app.post('/registration',(req , res)=>{
+    const user = new User(req.body);
+
+    user.save()
+    .then((result)=>{
+        res.redirect('/login');
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+});
+
+app.get('/adduser',(req,res)=>{
+    const user = new User({
+        username : "butter",
+        password : "butter123",
+        bloodgroup: "O+ve"
+    });
+
+    user.save()
+    .then((result)=>{
+        res.send(result);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 });
 
 app.use((req,res)=>{
---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    bloodgroup : {
+        type : String,
+        required : true
+    },
+    login : {
+        type : Boolean,
+        default : false
+    }
+},{timestamps : true});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
